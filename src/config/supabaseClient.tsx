af6f6629890d5f9467ba83420e5243a0a734bb67
millionseleshi <million2seleshi@gmail.com>
2023-12-01 00:04:00 -0700

    supabaseClient env configured

--- src/config/supabaseClient.tsx
+++ src/config/supabaseClient.tsx
@@ -1,7 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const  supabaseAnonKey = import.meta.env.VITE_REACT_APP_SUPABASE_URL as string
-const supabaseUrl = import.meta.env.VITE_REACT_APP_ANON_KEY  as string
-
+const supabaseAnonKey = import.meta.env.VITE_REACT_APP_ANON_KEY as string;
+const supabaseUrl = import.meta.env.VITE_REACT_APP_SUPABASE_URL as string;
 
 export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
