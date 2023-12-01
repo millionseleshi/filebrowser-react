932962d99bf93ed424ec6739ee8d78ffe1edf220
millionseleshi <million2seleshi@gmail.com>
2023-11-30 23:40:33 -0700

    add vite env

--- src/config/supabaseClient.tsx
+++ src/config/supabaseClient.tsx
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl: string = process.env.REACT_APP_SUPABASE_URL!
-const supabaseAnonKey: string = process.env. REACT_APP_SUPABASE_ANON_KEY!;
+const  supabaseAnonKey = import.meta.env.VITE_REACT_APP_SUPABASE_URL as string
+const supabaseUrl = import.meta.env.VITE_REACT_APP_ANON_KEY  as string
 
 
 export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
